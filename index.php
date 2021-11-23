88ea9036f4da7bee33e7b7e4892fdc99dc21bd71
shishlemek <83347720+shishlemek@users.noreply.github.com>
2021-11-23 13:43:13 +0200

    Added form

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <center>
+    <form>
+      Email:
+      <input type="text" name="e-mail" placeholder="Enter your e-mail">
+      <br><br>
+      Password:
+      <input type="password" name="password" placeholder="Enter your password">
+      <br><br>
+      <input type="submit" name="submit"value="Submit">
+    </form>
+  </center>
+  </body>
+</html>
